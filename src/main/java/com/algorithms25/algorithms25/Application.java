e40e5cc9c93c6bd30d4474ccccca0d7cd6aaa57f
1310098 <nail.abdulin.1993@mail.ru>
2022-11-09 14:28:05 +0300

    Initial commit

---
+++ src/main/java/com/algorithms25/algorithms25/Application.java
@@ -0,0 +1,13 @@
+package com.algorithms25.algorithms25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
